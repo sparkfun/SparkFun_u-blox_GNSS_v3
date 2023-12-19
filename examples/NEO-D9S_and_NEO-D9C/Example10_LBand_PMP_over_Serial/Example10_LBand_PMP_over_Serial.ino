027040c3a9569210248aed28569b6ac09d9ec9e8
Paul <pc235603@gmail.com>
2023-12-19 05:26:51 +0000

    Default to US frequency

--- examples/NEO-D9S_and_NEO-D9C/Example10_LBand_PMP_over_Serial/Example10_LBand_PMP_over_Serial.ino
+++ examples/NEO-D9S_and_NEO-D9C/Example10_LBand_PMP_over_Serial/Example10_LBand_PMP_over_Serial.ino
@@ -25,8 +25,8 @@ HardwareSerial serialGNSS(2); // TX on 17, RX on 16
 #include <SparkFun_u-blox_GNSS_v3.h> //http://librarymanager/All#SparkFun_u-blox_GNSS_v3
 SFE_UBLOX_GNSS myLBand; // NEO-D9S
 
-//const uint32_t myLBandFreq = 1556290000; // Uncomment this line to use the US SPARTN 1.8 service
-const uint32_t myLBandFreq = 1545260000; // Uncomment this line to use the EU SPARTN 1.8 service
+const uint32_t myLBandFreq = 1556290000; // Uncomment this line to use the US SPARTN 1.8 service
+//const uint32_t myLBandFreq = 1545260000; // Uncomment this line to use the EU SPARTN 1.8 service
 
 #define OK(ok) (ok ? F("  ->  OK") : F("  ->  ERROR!")) // Convert uint8_t into OK/ERROR
 
